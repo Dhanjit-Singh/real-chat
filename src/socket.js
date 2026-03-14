84bafd037ad5dbe9cae3fa04e9bb8fa144935949
Dhanjit-Singh <dhanjit.nrit@gmail.com>
2026-03-14 15:54:51 +0530

    backend url updated

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,9 @@
 import { io } from "socket.io-client";
 
 // const socket = io("http://localhost:5000");
-const socket = io("https://real-chat-backend-c3nm.onrender.com");
+// const socket = io("https://real-chat-backend-c3nm.onrender.com");
+const socket = io("https://real-chat-backend-c3nm.onrender.com", {
+    transports: ["websocket"]
+});
 
 export default socket;
