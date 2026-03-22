28ca55dd78b74c45a550c64bb2bb47c01558c9b3
Dhanjit-Singh <dhanjit.nrit@gmail.com>
2026-03-22 23:49:28 +0530

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,31 +1,3 @@
-// import { initializeApp } from "firebase/app";
-// import { getMessaging, onBackgroundMessage } from "firebase/messaging/sw";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBL65bsQsRSScRKKRP5ev9Jh7Hj3zVbMlA",
-//   authDomain: "real-chat-7038a.firebaseapp.com",
-//   projectId: "real-chat-7038a",
-//   messagingSenderId: "1042387637315",
-//   appId: "1:1042387637315:web:27af360f7900e88cd830f6"
-// };
-
-// const app = initializeApp(firebaseConfig);
-// const messaging = getMessaging(app);
-
-// onBackgroundMessage(messaging, (payload) => {
-//   console.log("[firebase-messaging-sw.js] Received background message ", payload);
-
-//   self.registration.showNotification(payload.notification.title, {
-//     body: payload.notification.body,
-//     icon: "/logo192.png",
-//   });
-// });
-
-
-
-
-// ✅ Use compat version (NO import)
-
 importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
 
@@ -37,31 +9,27 @@ const firebaseConfig = {
   appId: "1:1042387637315:web:27af360f7900e88cd830f6",
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Messaging instance
 const messaging = firebase.messaging();
 
-// Background notifications
 messaging.onBackgroundMessage(function (payload) {
   console.log("Background message:", payload);
 
   self.registration.showNotification(payload.notification.title, {
     body: payload.notification.body,
-    icon: "/real-chat/logo192.png", // important for GitHub Pages
+    icon: "/logo192.png",
   });
 });
 
 self.addEventListener("push", function (event) {
   console.log("Push received:", event);
-
   if (!event.data) return;
 
   const data = event.data.json();
 
   self.registration.showNotification(data.notification.title, {
     body: data.notification.body,
-    icon: "/real-chat/logo192.png",
+    icon: "/logo192.png",
   });
 });
